a08aaa9ce7cae82aee420d08a4dcb48bbd0a80f4
🎈 <numofran6@gmail.com>
2022-09-08 10:17:09 +0000

    renders ErrorMessage Component in Formik

---
+++ FormikControl.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Checkbox from './Checkbox';
+import Input from './Input';
+import Radio from './Radio';
+import Select from './Select';
+import TextArea from './TextArea';
+
+function FormikControl(props) {
+	const { control, ...rest } = props;
+
+	switch (control) {
+		case 'input':
+			return <Input {...rest} />;
+		case 'textarea':
+			return <TextArea {...rest} />;
+		case 'select':
+			return <Select {...rest} />;
+		case 'radio':
+			return <Radio {...rest} />;
+		case 'checkbox':
+			return <Checkbox {...rest} />;
+		default:
+			return null;
+	}
+}
+
+export default FormikControl;
